28d91a95013c2c001e3ce7a47c4c61231e344ff2
Evgeniya Polyakova <evgeniyapolyakova050000@yandex.ru>
2022-10-19 16:28:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const playButton = document.querySelector(".sputnik24-playing-toggle");
+
+playButton.addEventListener("click", () => {
+  playButton.classList.toggle("sputnik24-play");
+  playButton.classList.toggle("sputnik24-pause");
+});
+
+const fullscreenButton = document.querySelector(".sputnik24-fullscreen-toggle");
+
+fullscreenButton.addEventListener("click", () => {
+  fullscreenButton.classList.toggle("sputnik24-fullscreen");
+  fullscreenButton.classList.toggle("sputnik24-exit-fullscreen");
+});
+
+const volumeRange = document.querySelector(".sputnik24-volume-range");
+const volumeProgress = document.querySelector(".sputnik24-volume-progress");
+const volumeIcon = document.querySelector(".volume-icon");
+
+volumeRange.addEventListener("input", () => {
+  volumeProgress.style.width = volumeRange.value + "%";
+
+  if (+volumeRange.value === 0) {
+    volumeIcon.classList.add("mute-volume");
+    volumeIcon.classList.remove("low-volume");
+  } else if (+volumeRange.value <= 50) {
+    volumeIcon.classList.remove("mute-volume");
+    volumeIcon.classList.add("low-volume");
+  } else {
+    volumeIcon.classList.remove("low-volume");
+  }
+});
+
+const volumePanel = document.querySelector(".sputnik24-volume-panel");
+const volumeArea = document.querySelector(".sputnik24-volume-area");
+
+volumeArea.addEventListener("mouseenter", function () {
+  // volumeControl.style.margin = '0px 2px 0px 0px';
+  volumePanel.style.width = "52px";
+});
+
+volumeArea.addEventListener("mouseleave", function () {
+  // volumeControl.style.margin = '0px 0px 0px 0px';
+  volumePanel.style.width = "0px";
+});
